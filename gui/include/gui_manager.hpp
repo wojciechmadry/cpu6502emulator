c34aa4f21909ceb4335caec1b517ba9d01356e88
Wojciech Madry <madrywojciech99@gmail.com>
2022-07-25 20:48:13 +0200

    Initialise cpu components and attach them to gui manager

--- gui/include/gui_manager.hpp
+++ gui/include/gui_manager.hpp
@@ -1,12 +1,22 @@
 #ifndef GUI_MANAGER_HPP
 #define GUI_MANAGER_HPP
 
-#include "qt_gui.h"
+#include <functional>
 
-class GuiManager : public Ui_cpu6502_mainwindow
+#include "memory.hpp"
+#include "qt_gui.h"
+#include "cpu.hpp"
+#include "interpreter.hpp"
+
+class GuiManager : public Ui::cpu6502_mainwindow
 {
     public:
+    GuiManager(cpu6502::CPU& cpu, cpu6502::interpreter::Interpreter& interpreter, cpu6502::Memory& memory) noexcept;
 
+    private:
+    std::reference_wrapper<cpu6502::CPU> m_cpu;
+    std::reference_wrapper<cpu6502::interpreter::Interpreter> m_interpreter;
+    std::reference_wrapper<cpu6502::Memory> m_memory;
 };
 
 #endif  // GUI_MANAGER_HPP
