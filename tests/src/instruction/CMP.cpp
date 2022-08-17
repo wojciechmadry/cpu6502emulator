f2eecdd5ea16a9247c6ea9f085f2c0a7fcbddc2d
Wojciech Madry <madrywojciech99@gmail.com>
2022-08-17 16:34:02 +0200

    Add more tests to CMP instruction

--- tests/src/instruction/CMP.cpp
+++ tests/src/instruction/CMP.cpp
@@ -20,9 +20,9 @@ namespace CPU6502_TEST::inner{
         //ASSERT CMP - Immediate
         opcode = static_cast<decltype(opcode)>(cpu6502::opcode::CMP::Immediate); // 2 cycles
 
-        for(std::int16_t i = -25 ; i <= 25 ; ++i)
+        for(std::int16_t i = -100 ; i <= 100 ; ++i)
         {
-            for(std::int16_t j = -25; j <= 25 ; ++j)
+            for(std::int16_t j = -100; j <= 100 ; ++j)
             {
                 // i -> acu
                 // j -> memory
@@ -144,6 +144,40 @@ namespace CPU6502_TEST::inner{
 
         //ASSERT CMP - Absolute
         opcode = static_cast<decltype(opcode)>(cpu6502::opcode::CMP::Absolute);
+
+        for(std::int16_t i = -15 ; i <= 15 ; ++i)
+        {
+            for(std::int16_t j = -15; j <= 15 ; ++j)
+            {
+                // i -> acu
+                // j -> memory
+                const auto carry_flag = static_cast<cpu6502::Byte>(i) >= static_cast<cpu6502::Byte>(j);
+                const auto zero_flag = i == j;
+                const auto result = static_cast<cpu6502::Byte>(i - j);
+                const auto negative_flag = static_cast<bool>(result & 0x80);
+                // Load i to ACU
+                utils::load_to_acu(cpu, static_cast<cpu6502::Byte>(i));
+                PC = cpu.get_registers().PC.get();
+
+                // Compare ACU with J (memory)
+                 mem[PC++] = opcode;
+                mem[PC++] = 0x41;
+                mem[PC++] = 0x47;
+                mem[0x4741] = static_cast<cpu6502::Byte>(j);
+                cpu.execute(4);
+
+                // Check Negative flag
+                all_good &= cpu.get_registers().PS.get(PSFlags::NegativeFlag) == negative_flag;
+
+                // Check Carry Flag
+                all_good &= cpu.get_registers().PS.get(PSFlags::CarryFlag) == carry_flag;
+
+                // Check Zero Flag
+                all_good &= cpu.get_registers().PS.get(PSFlags::ZeroFlag) == zero_flag;
+
+                utils::jump_to_2020(cpu);
+            }
+        }
         
         utils::load_to_acu(cpu, 30);
         PC = cpu.get_registers().PC.get();
