e2ebc8fb2cdbaf7a3f46929fd48d253d65e80081
Wojciech Madry <madrywojciech99@gmail.com>
2022-12-21 17:55:38 +0100

    Integrate clang-tidy and build Debug program
    Signed-off-by: Wojciech Madry <madrywojciech99@gmail.com>

--- cpu6502/cpu/InstructionSet/Arithmetic/ADC.cpp
+++ cpu6502/cpu/InstructionSet/Arithmetic/ADC.cpp
@@ -19,8 +19,8 @@ namespace cpu6502{
         reg.PS.set(CPU::PSFlags::ZeroFlag, reg.ACU.get() == 0);
         reg.PS.set(CPU::PSFlags::NegativeFlag, reg.ACU.get() & 0x80);
 
-        bool is_overflow = !(ACU & 0x80) && !(value & 0x80) && word_data & 0x80
-                        || (ACU & 0x80) && (value & 0x80) && !(word_data & 0x80);
+        bool is_overflow = (!(ACU & 0x80) && !(value & 0x80) && word_data & 0x80)
+                        || ((ACU & 0x80) && (value & 0x80) && !(word_data & 0x80));
 
         reg.PS.set(CPU::PSFlags::OverflowFlag, is_overflow);
         reg.PS.set(CPU::PSFlags::CarryFlag, word_data > std::numeric_limits<Byte>::max());
